87538e9eaa17ffbc12a48d030bb834c80c2e5f25
Yasmine Mohamed <yasmineraef76@gmail.com>
2026-06-17 13:59:45 -0400

    Added function to retrieve pokemon data based on ID

--- index.js
+++ index.js
@@ -3,24 +3,37 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello :)");
+  res.send("Hello :)");
 });
 
 const name = "Pikachu";
 
-const getApi = async (name) => {
-	const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-	const parseUrl = await url.json();
-	console.log(parseUrl.id);
-	// const gameId = Number(parseUrl.id);
-	const coded = btoa(parseUrl.id);
-	const decoded = atob(coded);
-	console.log(coded);
-	console.log(decoded);
+const getApi = async () => {
+  const url = await fetch(`https://pokeapi.co/api/v2/generation/1`);
+  const parseUrl = await url.json();
+  console.log(parseUrl);
+  //   const coded = btoa(parseUrl.id);
+  //   //   const decoded = atob(coded);
+  //   console.log(coded);
+  //   return coded;
+  //   console.log(decoded);
+};
+//23
+const getID = async (number) => {
+  const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+  const parsedURL = await url.json();
+  console.log(number);
+  console.log(parsedURL.name);
 };
 
-getApi(name);
+getID(Math.floor(Math.random() * 10));
+
+app.get("/new", (req, res) => {
+  res.send(`Listening, this is the data:`);
+});
 
 app.listen(PORT, () => {
-	console.log(`Listening at Port: 3000`);
+  console.log(`Listening at Port: 3000`);
 });
+
+console.log();
