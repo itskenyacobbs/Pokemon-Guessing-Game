4b527de7f0e20ee045b82493e44914f48040b297
Kenya <kenyacobbs9001@gmail.com>
2026-06-17 13:22:09 -0400

    decoded feature

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get("/", (req, res) => {
 	res.send("Hello :)");
 });
 
+const name = "Pikachu";
+
+const getApi = async (name) => {
+	const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+	const parseUrl = await url.json();
+	console.log(parseUrl.id);
+	// const gameId = Number(parseUrl.id);
+	const coded = btoa(parseUrl.id);
+	const decoded = atob(coded);
+	console.log(coded);
+	console.log(decoded);
+};
+
+getApi(name);
+
 app.listen(PORT, () => {
 	console.log(`Listening at Port: 3000`);
 });
