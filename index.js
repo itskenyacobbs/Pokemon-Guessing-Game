47790c976d5bb0efe2adfa2e114dc32980a420df
Kenya <kenyacobbs9001@gmail.com>
2026-06-18 13:00:03 -0400

    added length checker in the decoded function to test the string before decoding; modified the random index of pokemonIds to the proper maximum of 1025

--- index.js
+++ index.js
@@ -5,69 +5,76 @@ const PORT = 3000;
 //------------------------------ METHODS FOR EXTERNAL API EXTRACTION ---------------------------------
 
 const decodeID = (encodedID) => {
-  return atob(encodedID);
+	console.log(encodedID);
+	if (encodedID.length !== 4) {
+		return "0";
+	}
+	return atob(encodedID);
 };
 
 //To retreive the specific pokemon information for a pokemon by it's id 'i.e number'
 const getPokemon = async (number) => {
-  const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
-  const pokemonData = await url.json();
-  console.log(number);
-  console.log("Pokemon name: ", pokemonData.name);
-  return pokemonData; //The whole pokemon data is returned
+	const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+	const pokemonData = await url.json();
+	console.log(number);
+	console.log("Pokemon name: ", pokemonData.name);
+	return pokemonData; //The whole pokemon data is returned
 };
 
 //Generating Pokemon hints
 const getHints = (pokemonData) => {
-  //!TODO: for pokemon data when retrieving hints --> 5?
-  return [
-    { height: pokemonData.height },
-    { cryLink: pokemonData.cries.latest },
-    { weight: pokemonData.weight },
-    { abilities: pokemonData.abilities[0].ability.name },
-    { image: pokemonData.sprites.front_default },
-  ];
+	//!TODO: for pokemon data when retrieving hints --> 5?
+	return [
+		{ height: pokemonData.height },
+		{ cryLink: pokemonData.cries.latest },
+		{ weight: pokemonData.weight },
+		{ abilities: pokemonData.abilities[0].ability.name },
+		{ image: pokemonData.sprites.front_default },
+	];
 };
 
 //
 const generateGameID = async () => {
-  const randomNumber = Math.floor(Math.random() * 1350) + 1;
-  const randomPokemon = await getPokemon(randomNumber);
-  const gameID = btoa(randomPokemon.id.toString());
-  console.log(gameID);
-  const hintObject = getHints(randomPokemon);
-  console.log(hintObject);
-  return { game_id: gameID, hints: 5 };
+	const randomNumber = Math.floor(Math.random() * 1024) + 1;
+	const randomPokemon = await getPokemon(randomNumber);
+	const gameID = btoa(randomPokemon.id.toString());
+	console.log(gameID);
+	const hintObject = getHints(randomPokemon);
+	console.log(hintObject);
+	return { game_id: gameID, hints: 5 };
 };
 
 //--------------------------- EXPRESS ROUTES ----------------------------------
 
 //Default route http://localhost:3000/
 app.get("/", (req, res) => {
-  res.send("Hello :)");
+	res.send("Hello :)");
 });
 
 app.get("/new", async (req, res) => {
-  const game = await generateGameID();
-  res.json(game);
+	const game = await generateGameID();
+	res.json(game);
 });
 
 app.get("/guess/:gameID/:n", async (req, res) => {
-  const { gameID, n } = req.params;
-  const decodeGameID = decodeID(gameID);
-  const game_id = Number(decodeGameID);
-  const hintIndex = Number(n);
-  console.log(game_id);
-  console.log(hintIndex);
-  const pokemon = await getPokemon(game_id);
-  const hints = getHints(pokemon);
-  const hint = hints[hintIndex];
-  console.log(hint);
-  res.json({ hint: hint });
+	const { gameID, n } = req.params;
+	const decodeGameID = decodeID(gameID);
+	const game_id = Number(decodeGameID);
+	if (!Number(decodeGameID)) {
+		res.status(400).json({ message: "invalid game id" });
+	}
+	const hintIndex = Number(n);
+	console.log(game_id);
+	console.log(hintIndex);
+	const pokemon = await getPokemon(game_id);
+	const hints = getHints(pokemon);
+	const hint = hints[hintIndex];
+	console.log(hint);
+	res.json({ hint: hint });
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening at Port: 3000`);
+	console.log(`Listening at Port: 3000`);
 });
 
 //---------- REUSABLE CODE -------------------
